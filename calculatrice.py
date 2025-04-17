5edee7be585dfa34b3988dafbb2abb7e920536ef
Bakary <50413259+Bakary98@users.noreply.github.com>
2025-04-17 16:54:57 +0200

    Create calculatrice.py

---
+++ calculatrice.py
@@ -0,0 +1,9 @@
+# Calculatrice 
+
+
+n1 = input("Veuillez entrer votre premier nombre: ")
+n2 = input("Veuillez entrer votre deuxieme nombre")
+
+total = int(n1) + int(n2)
+
+print(f"La somme de {n1} et de {n2} font au total {total}")
