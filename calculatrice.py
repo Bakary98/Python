2702ca290ab27f1d6d7dcf77a2265207426fe4a9
Bakary <50413259+Bakary98@users.noreply.github.com>
2025-04-25 12:19:11 +0200

    Améliorer Calculatrice

--- calculatrice.py
+++ calculatrice.py
@@ -1,9 +1,24 @@
 # Calculatrice 
 
-
+# Première version
 n1 = input("Veuillez entrer votre premier nombre: ")
 n2 = input("Veuillez entrer votre deuxieme nombre")
 
 total = int(n1) + int(n2)
 
 print(f"La somme de {n1} et de {n2} font au total {total}")
+
+# Deuxième version
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+n1 = n2 = ""
+
+while not(n1.isdigit() and n2.isdigit()):
+   
+    n1 = input(" Entrez un premier nombre premier nombre: ")
+    n2 = input(" Entrez un deuxieme nombre: ")
+    if not(n1.isdigit() and n2.isdigit()):
+        print(f"Veuillez entrer deux nombres valides")
+else : 
+   
+   print(f"La somme de {n1} et de {n2} font au total {int(n1) + int(n2)}")
